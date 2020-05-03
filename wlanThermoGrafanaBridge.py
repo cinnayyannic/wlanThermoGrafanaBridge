03cdb8b54d90844ad69e9679953ab84e8c1d02ca
Yannic <Yannic@Yannic-Thinkpad>
2020-05-04 00:25:00 +0200

    added decoding of system settings

--- wlanThermoGrafanaBridge.py
+++ wlanThermoGrafanaBridge.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import copy
 import logging
 import json
 import paho.mqtt.client as mqtt
@@ -8,8 +9,40 @@ from datetime import datetime
 
 log = logging.getLogger('WlanThermoGrafanaBridge')
 
-def createSystemPoints(points, timestamp, message):
-    log.debug('Createing system points from json payload')
+class GlobalScope:
+    influxDbClient = None
+    replicateSettings = None
+    lastSettingsPoints = []
+
+def getTimestampStr(unixTimestamp):
+    timestamp = None
+    if unixTimestamp < 631152000: # 01.01.1990 00:00:00
+        # internal system time of wlanthermo starts counting from 0 until external time is provided
+        # we assume the system up time will never reach 631152000 on its own
+        # if internal time is detected we use receive timestamps instead
+        timestamp = datetime.utcnow()
+        log.debug('Wlanthermo running on internal time > using receive timestamps')
+    else:
+        timestamp = datetime.fromtimestamp(unixTimestamp)
+
+    timestampStr = timestamp.astimezone().replace(microsecond=0).isoformat()
+
+    return timestampStr
+
+def prepareReplicatedPoints(points, dataUnixTimestamp):
+    newPoints = []
+    if len(points) > 0:
+        # deepcoy points to keep the original dataset
+        newPoints = copy.deepcopy(points)
+        # change timstamp from old points to new timestamps
+        timestampStr = getTimestampStr(dataUnixTimestamp)
+        for point in newPoints:
+            point['time'] = timestampStr
+
+    return newPoints
+
+def createDataSystemPoints(points, timestamp, message):
+    log.debug('Creating data[system] points from json payload')
 
     systemItem = {}
     systemItem['measurement'] = "System"
@@ -18,8 +51,8 @@ def createSystemPoints(points, timestamp, message):
 
     points.append(systemItem)
 
-def createChannelPoints(points, timestamp, message):
-    log.debug('Createing channel points from json payload')
+def createDataChannelPoints(points, timestamp, message):
+    log.debug('Creating data [channel] points from json payload')
 
     for channel in message['channel']:
         if channel['temp'] < 999: # 999 means channel not active
@@ -36,51 +69,69 @@ def createChannelPoints(points, timestamp, message):
 
             points.append(channelItem)
 
-def createPitmasterPoints(points, timestamp, message):
-    log.debug('Createing pitmaster points from json payload')
+def createDataPitmasterPoints(points, timestamp, message):
+    log.debug('Creating data [pitmaster] points from json payload')
 
-    pitmasterItem = {}
-    pitmasterItem['measurement'] = "Pitmaster"
-    pitmasterItem['time'] = timestamp
-    pitmasterItem['fields'] = message['pitmaster']
+    for pmObject in message['pitmaster']['pm']:
+        pitmasterItem = {}
+        pitmasterItem['measurement'] = "Pitmaster {}".format(pmObject['id'])
+        pitmasterItem['time'] = timestamp
+        pitmasterItem['fields'] = pmObject
 
-    points.append(pitmasterItem)
+        points.append(pitmasterItem)
+
+def createSettingsSystemPoints(points, timestamp, message):
+    log.debug('Creating settings [system] points from json payload')
+
+    systemItem = {}
+    systemItem['measurement'] = "System settings"
+    systemItem['time'] = timestamp
+    systemItem['fields'] = message['system']
+
+    points.append(systemItem)
 
 def addPointsToDatabase(influxDbClient, points):
     if len(points) > 0:
-        influxDbClient.write_points(points=points, database='wlanthermo')
-        log.info('InfluxDB datapoints inserted')
+        try:
+            influxDbClient.write_points(points=points, database='wlanthermo')
+            log.info('InfluxDB datapoints inserted')
+        except Exception as ex:
+            log.warning('InfluxDB insertion of datapoints failed')
+            log.debug(ex)
 
 def createDataPoints(message):
     points = []
 
     try:
-        unixTimestamp = int(message['system']['time'])
-        timestamp = None
+        timestampStr = getTimestampStr(int(message['system']['time']))
 
-        if unixTimestamp < 631152000: # 01.01.1990 00:00:00
-            # internal system time of wlanthermo starts counting from 0 until external time is provided
-            # we assume the system up time will never reach 631152000 on its own
-            # if internal time is detected we use receive timestamps instead
-            timestamp = datetime.utcnow()
-            log.debug('Wlanthermo running on internal time > using receive timestamps')
-        else:
-            timestamp = datetime.fromtimestamp(unixTimestamp)
+        createDataSystemPoints(points, timestampStr, message)
+        createDataChannelPoints(points, timestampStr, message)
+        createDataPitmasterPoints(points, timestampStr, message)
 
-        timestampStr = timestamp.astimezone().replace(microsecond=0).isoformat()
-
-        createSystemPoints(points, timestampStr, message)
-        createChannelPoints(points, timestampStr, message)
-        createPitmasterPoints(points, timestampStr, message)
-
-        log.debug('Created points from json payload')
+        log.debug('Created data points from json payload')
     except Exception as ex:
-        log.warning('Failed to create points from json payload')
+        log.warning('Failed to create data points from json payload')
         log.debug(ex)
 
     return points
 
-def mqtt_on_connect(client, influxDbClient, flags, rc):
+def createSettingsPoints(message):
+    points = []
+
+    try:
+        timestampStr = getTimestampStr(int(message['system']['time']))
+
+        createSettingsSystemPoints(points, timestampStr, message)
+
+        log.debug('Created settings points from json payload')
+    except Exception as ex:
+        log.warning('Failed to create settings points from json payload')
+        log.debug(ex)
+
+    return points
+
+def mqtt_on_connect(client, globalScope, flags, rc):
     if rc == 0:
         log.info('Connection to mqtt broker successfull')
         client.subscribe('WLanThermo/+/status/settings')
@@ -89,29 +140,44 @@ def mqtt_on_connect(client, influxDbClient, flags, rc):
     else:
         log.error('Connection to mqtt broker failed rc={}'.format(rc))
 
-def mqtt_on_message(client, influxDbClient, msg):
+def mqtt_on_message(client, globalScope, msg):
     decodedMessage = str(msg.payload.decode("utf-8","ignore"))
     log.info('Received unhandled mqtt message')
     log.debug(decodedMessage)
 
-def on_wlanthermo_data(client, influxDbClient, msg):
+def on_wlanthermo_data(client, globalScope, msg):
     log.info('Received wlanthermo mqtt data message')
     decodedMessage = str(msg.payload.decode("utf-8","ignore"))
     log.debug(decodedMessage)
-    messageJson = json.loads(decodedMessage)
-    points = createDataPoints(messageJson)
-    addPointsToDatabase(influxDbClient, points)
+    try:
+        messageJson = json.loads(decodedMessage)
+        points = createDataPoints(messageJson)
+        addPointsToDatabase(globalScope.influxDbClient, points)
+        if globalScope.replicateSettings and len(globalScope.lastSettingsPoints) > 0:
+            dataUnixTimestamp = int(messageJson['system']['time'])
+            replicatedPoints = prepareReplicatedPoints(globalScope.lastSettingsPoints, dataUnixTimestamp)
+            addPointsToDatabase(globalScope.influxDbClient, replicatedPoints)
+            log.info('Replicated last settings object')
+    except Exception as ex:
+        log.warning('Failed to parse json payload')
+        log.debug(ex)
 
-def on_wlanthermo_settings(client, influxDbClient, msg):
+def on_wlanthermo_settings(client, globalScope, msg):
     decodedMessage = str(msg.payload.decode("utf-8","ignore"))
     log.info('Received wlanthermo mqtt settings message')
     log.debug(decodedMessage)
-    # messageJson = json.loads(decodedMessage)
-    # points = createSettingsPoints(messageJson)
-    # addPointsToDatabase(influxDbClient, points)
+    try:
+        messageJson = json.loads(decodedMessage)
+        points = createSettingsPoints(messageJson)
+        globalScope.lastSettingsPoints = points
+        addPointsToDatabase(globalScope.influxDbClient, points)
+    except Exception as ex:
+        log.warning('Failed to parse json payload')
+        log.debug(ex)
 
 def main(argv):
     mqtt_client_name = "wlanThermoGrafanaBridge"
+    globalScope = GlobalScope()
 
     parser = argparse.ArgumentParser(description='WlanThermoGrafanaBridge')
     parser.add_argument('--mqttHost', metavar='<mqtt hostname>', type=str, default='localhost', help='MQTT host')
@@ -123,6 +189,7 @@ def main(argv):
     parser.add_argument('--influxDbUsername', metavar='<influxDB username>', type=str, default='', help='InfluxDB username')
     parser.add_argument('--influxDbPassword', metavar='<influxDB password>', type=str, default='', help='InfluxDB password')
     parser.add_argument('--influxDbName', metavar='<influxDB name>', type=str, default='wlanthermo', help='InfluxDB database name')
+    parser.add_argument('--replicateSettings', default=False, action='store_true', help='Replicate last seetings object with each data measurement')
     parser.add_argument('--logLevel', metavar='<log level>', type=str, choices=['debug', 'info', 'warning', 'error'], default='info', help='Set Log level')
     parser.add_argument('--logFile', metavar='<log file filepath>', type=str, help='Logile output file')
 
@@ -142,10 +209,12 @@ def main(argv):
 
     logging.basicConfig(level=logLevels.get(args.logLevel, logging.INFO), filename=args.logFile)
 
+    globalScope.replicateSettings = args.replicateSettings
+
     influxDbClient = InfluxDBClient(host=args.influxDbHost, port=args.influxDbPort,
         username=args.influxDbUsername, password=args.influxDbPassword, database=args.influxDbName)
 
-    mqttClient = mqtt.Client(client_id=mqtt_client_name, clean_session=True, userdata=influxDbClient)
+    mqttClient = mqtt.Client(client_id=mqtt_client_name, clean_session=True, userdata=globalScope)
     mqttClient.username_pw_set(args.mqttUsername, args.mqttPassword)
     mqttClient.on_connect = mqtt_on_connect
     mqttClient.on_message = mqtt_on_message
@@ -155,7 +224,8 @@ def main(argv):
 
     try:
         log.info('Connect to InfluxDB {}:{}'.format(args.influxDbHost, args.influxDbPort))
-        influxDbClient.create_database('wlanthermo')
+        influxDbClient.create_database(args.influxDbName)
+        globalScope.influxDbClient = influxDbClient
         log.info('Connect to InfluxDB successful')
     except Exception as ex:
         log.error('Connection to InfluxDB failed')
